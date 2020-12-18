71c4e863fb4c832b380b8ce8fd1ec3db759d2e9d
elnathanau1 <elnathanau1@gmail.com>
2020-12-18 12:00:31 -0600

    init repo

---
+++ scrapers/master-scraper.py
@@ -0,0 +1,165 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+import logging
+import datetime
+import json
+import csv
+
+
+ESPN_API_URL = "https://fantasy.espn.com/apis/v3/games/fba/seasons/2021/segments/0/leaguedefaults/1?view=kona_player_info"
+HASHTAG_RANKINGS_URL = "https://hashtagbasketball.com/fantasy-basketball-projections"
+CNN_INJURIES_URL = "https://www.cbssports.com/nba/injuries/"
+
+
+def pull_data_espn():
+    # grab the page - note verify=False is not safe
+    headers = {'x-fantasy-filter' : '{"players":{"filterSlotIds":{"value":[0,1,2,3,4,5,6,7,8,9,10,11]},"limit":1013,"offset":0,"sortAdp":{"sortAsc":true,"sortPriority":1},"sortDraftRanks":{"sortPriority":100,"sortAsc":true,"value":"STANDARD"},"filterRanksForScoringPeriodIds":{"value":[1]},"filterRanksForRankTypes":{"value":["STANDARD"]},"filterStatsForTopScoringPeriodIds":{"value":5,"additionalValue":["002021","102021","002020","012021","022021","032021","042021"]}}}'}
+    page = requests.get(ESPN_API_URL, headers=headers)
+    players_json = json.loads(page.text)
+
+    player_list = []
+    for player in players_json['players']:
+        try:
+            name = player['player']["fullName"].replace(".", "")
+            rank = player['player']["draftRanksByRankType"]["STANDARD"]["rank"]
+            adp = player['player']["ownership"]["averageDraftPosition"]
+            eligibleSlots = player['player']['eligibleSlots']
+
+            slotToPosition = {
+                0 : 'PG',
+                1 : 'SG',
+                2 : 'SF',
+                3 : 'PF',
+                4 : 'C'
+            }
+            positions = []
+            for slot in slotToPosition.keys():
+                if slot in eligibleSlots:
+                    positions.append(slotToPosition[slot])
+
+            formatted_data = [rank, name, adp, positions]
+            player_list.append(tuple(formatted_data))
+        except:
+            continue
+
+    return player_list
+
+
+def pull_data_hashtag_zscores():
+    # grab the page - note verify=False is not safe
+    page = requests.get(HASHTAG_RANKINGS_URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    players_div = soup.findAll("div", {"class" : "table-responsive"})[2]
+    rankings_table = players_div.findAll("tr")
+
+    player_list = []
+    for i in range(0, len(rankings_table)):
+        row = rankings_table[i]
+        td = row.findAll("td")
+        if len(td) == 0:
+            continue
+        else:
+            rank = td[0].text.strip()
+            if rank != "R#":
+                # unspan the tags that have it (not all of them do)
+                formatted_data = [""] * 13
+                formatted_data[0] = rank
+                k = 1
+                for j in range(0, len(td)):
+                    if td[j].find("span") != None and td[j].find("input") == None:
+                        if k < len(formatted_data) - 1:     # so that we don't pull total, we'll calculate that ourselves
+                            formatted_data[k] = td[j].find("span").text.strip()
+                            k += 1
+
+                    elif len(td[j].findAll("input")) == 2:
+                        if k < len(formatted_data) - 1:
+                            formatted_data[k] = td[j].findAll("input")[1]['value'].strip()
+                            k += 1
+                tip = row.find("span", {"class" : "tip-content"})
+                if tip is not None:
+                    formatted_data[len(formatted_data) - 1] = tip.text.strip()
+
+                player_list.append(tuple(formatted_data))
+
+    return player_list
+
+def pull_data_cnn_injuries():
+    page = requests.get(CNN_INJURIES_URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    page_content = soup.find('div', { 'class' : 'Page-content'})
+    all_tr = page_content.findAll('tr', { 'class' : 'TableBase-bodyTr'})
+
+    player_list = []
+    for tr in all_tr:
+        full_name = tr.find('span', { 'class' : 'CellPlayerName--long'}).text.strip().replace(".", "")
+        injury = tr.findAll('td')[-1].text.strip()
+
+        player_list.append(tuple([full_name, injury]))
+
+    return player_list
+
+
+def combined_rankings(espn_rankings, hashtag_rankings_zscore, cnn_injuries):
+    player_list = []
+    for player in hashtag_rankings_zscore:
+        rank = player[0]
+        player_name = player[2].replace(".", "")
+        fg = round(float(player[3]), 2)
+        ft = round(float(player[4]), 2)
+        three = round(float(player[5]), 2)
+        pts = round(float(player[6]), 2)
+        reb = round(float(player[7]), 2)
+        ast = round(float(player[8]), 2)
+        stl = round(float(player[9]), 2)
+        blk = round(float(player[10]), 2)
+        to = round(float(player[11]), 2) * -1.0
+        tip = player[12]
+
+        espn_player = next((x for x in espn_rankings if x[1] == player_name), [500, "", 150, []])
+        espn_rank = espn_player[0]
+        espn_adp = round(float(espn_player[2]), 2)
+        espn_positions = espn_player[3]
+
+        cnn_player = next((x for x in cnn_injuries if x[0] == player_name), ["", ""])
+        injury = cnn_player[1]
+
+        player_list.append({
+            'rank' : rank,
+            'name' : player_name,
+            'espn_rank' : espn_rank,
+            'espn_adp' : espn_adp,
+            'fg' : fg,
+            'ft' : ft,
+            'three' : three,
+            'pts' : pts,
+            'reb' : reb,
+            'ast' : ast,
+            'stl' : stl,
+            'blk' : blk,
+            'to' : to,
+            'tip' : tip,
+            'injury' : injury,
+            'positions' : espn_positions
+        })
+
+    return player_list
+
+
+def outputJson(combined_rankings):
+    with open('rankings.json', 'w+') as outfile:
+        json.dump(combined_rankings, outfile)
+
+if __name__ == '__main__':
+    espn_rankings = pull_data_espn()
+    hashtag_rankings_zscore = pull_data_hashtag_zscores()
+    cnn_injuries = pull_data_cnn_injuries()
+
+    combined_rankings = combined_rankings(espn_rankings, hashtag_rankings_zscore, cnn_injuries)
+
+    outputJson(combined_rankings)
+    # with open('rankings.csv', 'w+') as file:
+    #     csv_writer = csv.writer(file, delimiter=',')
+    #     csv_writer.writerows(combined_rankings)
---
+++ ui/src/index.js
@@ -0,0 +1,434 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import { MDBContainer, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBRow, MDBCol, MDBDataTable, MDBBtn, MDBInput, MDBTooltip, MDBTable, MDBTableHead, MDBTableBody, MDBIcon} from "mdbreact";
+import "./index.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modal_team_overview : false,
+      modal_tiers_pg : false,
+      modal_tiers_sg : false,
+      modal_tiers_sf : false,
+      modal_tiers_pf : false,
+      modal_tiers_c : false,
+      rankings : [],
+      multiplier : {
+        fg : 1.0,
+        ft : 1.0,
+        three : 1.0,
+        pts : 1.0,
+        ast : 1.0,
+        reb : 1.0,
+        stl : 1.0,
+        blk : 1.0,
+        to : 0.25
+      },
+      teams : [ [], [], [], [], [], [], [], [], [], [], [], [] ],
+      team_totals : [
+        { name : "S101 S. 6th St Brick", fg : 0, ft : 0, three : 0, pts : 0, reb : 0, ast : 0, stl : 0, blk : 0, to : 0 },
+        { name : "Team Au", fg : 0, ft : 0, three : 0, pts : 0, reb : 0, ast : 0, stl : 0, blk : 0, to : 0 },
+        { name : "Team Lebran", fg : 0, ft : 0, three : 0, pts : 0, reb : 0, ast : 0, stl : 0, blk : 0, to : 0 },
+        { name : "LigMA Ballyers", fg : 0, ft : 0, three : 0, pts : 0, reb : 0, ast : 0, stl : 0, blk : 0, to : 0 },
+        { name : "Team JON", fg : 0, ft : 0, three : 0, pts : 0, reb : 0, ast : 0, stl : 0, blk : 0, to : 0 },
+        { name : "Team Orr", fg : 0, ft : 0, three : 0, pts : 0, reb : 0, ast : 0, stl : 0, blk : 0, to : 0 },
+        { name : "zoom university Big Wong", fg : 0, ft : 0, three : 0, pts : 0, reb : 0, ast : 0, stl : 0, blk : 0, to : 0 },
+        { name : "Hoop Squad", fg : 0, ft : 0, three : 0, pts : 0, reb : 0, ast : 0, stl : 0, blk : 0, to : 0 },
+        { name : "RIP KOBE MELO = GOAT", fg : 0, ft : 0, three : 0, pts : 0, reb : 0, ast : 0, stl : 0, blk : 0, to : 0 },
+        { name : "Team Choi", fg : 0, ft : 0, three : 0, pts : 0, reb : 0, ast : 0, stl : 0, blk : 0, to : 0 },
+        { name : "Team Git JLin back", fg : 0, ft : 0, three : 0, pts : 0, reb : 0, ast : 0, stl : 0, blk : 0, to : 0 },
+        { name : "Team Wood", fg : 0, ft : 0, three : 0, pts : 0, reb : 0, ast : 0, stl : 0, blk : 0, to : 0 }
+      ],
+      tiers : {
+        pg : [],
+        sg : [],
+        sf : [],
+        pf : [],
+        c : [],
+      },
+      overall_position : 0,
+      position : 1,
+      direction : "FORWARD"
+    };
+  }
+
+  positions = ['pg', 'sg', 'sf', 'pf', 'c'];
+  stat_fields = [ "fg" , "ft", "three", 'pts', 'reb', 'ast', 'stl', 'blk', 'to' ];
+
+  componentDidMount() {
+    this.fillTierList();
+  }
+
+  fillTierList = () => {
+    let newTiersState = {};
+    this.positions.forEach( pos => {
+      let json = require("./tiers_" + pos + ".json");
+      let newTiers = [];
+      json.tiers.forEach( tier => {
+        let newTier = [];
+        tier.forEach( player => {
+          if (!this.isDrafted(player)) {
+            newTier.push(player);
+          }
+        });
+
+        newTiers.push(newTier);
+      })
+      newTiersState[pos] = newTiers;
+    })
+
+    this.setState({
+      tiers : newTiersState
+    })
+
+  }
+
+  handleInputChange = inputName => value => {
+    this.setState({
+      multiplier : {
+        ...this.state.multiplier,
+        [inputName] : value
+      }
+    }, this.resetRankings);
+  };
+
+  isDrafted = name => {
+    for (var i = 0; i < this.state.teams.length; i++) {
+      var team = this.state.teams[i];
+      for (var j = 0; j < team.length; j++) {
+        if (team[j].name == name) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  nextDraftPos = () => {
+    if (this.state.direction == "FORWARD" && this.state.position == 12) {
+      this.setState({
+        overall_position : this.state.overall_position + 1,
+        direction : "BACKWARD"
+      })
+    }
+    else if (this.state.direction == "BACKWARD" && this.state.position == 1) {
+      this.setState({
+        overall_position : this.state.overall_position + 1,
+        direction : "FORWARD"
+      })
+    }
+    else if(this.state.direction == "FORWARD") {
+      this.setState({
+        overall_position : this.state.overall_position + 1,
+        position : this.state.position + 1
+      })
+    }
+    else if(this.state.direction == "BACKWARD") {
+      this.setState({
+        overall_position : this.state.overall_position + 1,
+        position : this.state.position - 1
+      })
+    }
+
+  }
+
+  handleClickDraft = player => () => {
+    this.draftPlayer(player);
+  }
+
+  draftPlayer = player => {
+    var teamIndex = this.state.position - 1;
+    var newTeams = this.state.teams;
+    newTeams[teamIndex].push(player);
+
+    var newTeamTotals = this.state.team_totals;
+    for (var i = 0; i < this.stat_fields.length; i++) {
+      var field = this.stat_fields[i];
+      newTeamTotals[teamIndex][field] = Math.round((newTeamTotals[teamIndex][field] + player[field]) * 100)/100;
+    }
+
+    this.setState({
+      teams : newTeams,
+      team_totals : newTeamTotals
+    }, () => {
+      this.nextDraftPos();
+      this.resetRankings();
+      this.fillTierList();
+    })
+  }
+
+  displayInjury = injury => {
+    if (injury != "") {
+      return (
+        <MDBTooltip placement="left" component="span" tooltipContent={injury}>
+          <MDBIcon far icon="plus-square" />
+        </MDBTooltip>
+      );
+    }
+    else {
+      return null;
+    }
+  }
+
+  resetRankings = () => {
+    let rankingsJson = require('./rankings.json');
+    var newRankings = [];
+    for (var i = 0; i < rankingsJson.length; i++) {
+      var total = 0;
+      for (var key in this.state.multiplier) {
+        total += (rankingsJson[i][key] * this.state.multiplier[key]);
+      };
+
+      newRankings.push( {
+        punt_rank : 0,
+        rank : parseInt(rankingsJson[i].rank),
+        name : rankingsJson[i].name,
+        injury : this.displayInjury(rankingsJson[i].injury),
+        position : rankingsJson[i].positions.toString(),
+        espn_adp : rankingsJson[i].espn_adp,
+        fg : rankingsJson[i].fg,
+        ft : rankingsJson[i].ft,
+        three : rankingsJson[i].three,
+        pts : rankingsJson[i].pts,
+        reb : rankingsJson[i].reb,
+        ast : rankingsJson[i].ast,
+        stl : rankingsJson[i].stl,
+        blk : rankingsJson[i].blk,
+        to : rankingsJson[i].to,
+        total : Math.round(total * 100)/100,
+        draft : <MDBTooltip placement="left" component="span" tooltipContent={rankingsJson[i].tip}><MDBBtn size="sm" onClick={this.handleClickDraft(rankingsJson[i])}>Draft</MDBBtn></MDBTooltip>
+      });
+    }
+
+    newRankings.sort((a, b) => (a.total <= b.total) ? 1 : -1);
+
+    var removedDrafted = [];
+    for (var i = 0; i < newRankings.length; i++) {
+      var player = newRankings[i];
+      if ( !this.isDrafted(player.name) ) {
+        player["punt_rank"] = i + 1;
+        removedDrafted.push(player);
+      }
+    }
+
+    this.setState( { rankings : removedDrafted } );
+  }
+
+  toggleModal = name => () => {
+    var field = "modal_" + name;
+    this.setState({
+      [field] : !this.state.[field]
+    });
+  };
+
+  render() {
+    const dataColumns = [
+      { label: 'PR' , field: 'punt_rank'},
+      { label: 'R' , field: 'rank'},
+      { label: 'Name' , field: 'name'},
+      { label: '', field: 'injury' },
+      { label: '', field: 'position'},
+      { label: 'ADP', field: 'espn_adp' },
+      { label: 'FG%' , field: 'fg'},
+      { label: 'FT%' , field: 'ft'},
+      { label: '3PM' , field: 'three'},
+      { label: 'PTS' , field: 'pts'},
+      { label: 'REB' , field: 'reb'},
+      { label: 'AST' , field: 'ast'},
+      { label: 'STL' , field: 'stl'},
+      { label: 'BLK' , field: 'blk'},
+      { label: 'TO' , field: 'to'},
+      { label: 'TOTAL', field: 'total' }
+    ];
+
+    const modalColumns = [
+      { label: 'Team' , field: 'name'},
+      { label: 'FG%' , field: 'fg'},
+      { label: 'FT%' , field: 'ft'},
+      { label: '3PM' , field: 'three'},
+      { label: 'PTS' , field: 'pts'},
+      { label: 'REB' , field: 'reb'},
+      { label: 'AST' , field: 'ast'},
+      { label: 'STL' , field: 'stl'},
+      { label: 'BLK' , field: 'blk'},
+      { label: 'TO' , field: 'to'}
+    ];
+
+    const positions = [
+      { name : 'Point Guard', abbreviation : 'pg' },
+      { name : 'Shooting Guard', abbreviation : 'sg' },
+      { name : 'Small Forward', abbreviation : 'sf' },
+      { name : 'Power Forward', abbreviation : 'pf' },
+      { name : 'Center', abbreviation : 'c' }
+    ];
+
+  return (
+      <React.Fragment>
+      <MDBContainer>
+        <MDBRow>
+          {positions.map((item, index) => (
+            <MDBBtn size="sm" color="secondary" onClick={this.toggleModal("tiers_" + item.abbreviation)}>{item.name}</MDBBtn>
+          ))}
+        </MDBRow>
+        <MDBRow>
+        <MDBCol md="9">
+          <MDBDataTable
+            autoWidth
+            hover
+            striped
+            bordered
+            hover
+            small
+            entries={50}
+            info={false}
+            order={['total', 'desc']}
+            data={ {columns : dataColumns, rows : this.state.rankings } }
+            refresh={this.state.rankings}
+            />
+          <MDBBtn onClick={ this.resetRankings }>New Draft</MDBBtn>
+        </MDBCol>
+        <MDBCol md="3">
+          Current Team: <br /> {this.state.team_totals[this.state.position-1].name} <br />
+          Overall position: {this.state.overall_position + 1} <br />
+          Round: {(Math.floor(this.state.overall_position/12) + 1)}, Pick: {this.state.overall_position % 12 + 1}<br />
+        <MDBBtn color="info" onClick={ this.toggleModal("team_overview") }>Team Overview</MDBBtn>
+          <form className="mx-3 grey-text">
+            <MDBInput
+              name="FG%"
+              label="fg%"
+              group
+              type="number"
+              valueDefault={this.state.multiplier.fg}
+              getValue={this.handleInputChange("fg")}
+            />
+            <MDBInput
+              name="FT%"
+              label="ft%"
+              group
+              type="number"
+              valueDefault={this.state.multiplier.ft}
+              getValue={this.handleInputChange("ft")}
+            />
+            <MDBInput
+              name="3PM"
+              label="3pm"
+              group
+              type="number"
+              valueDefault={this.state.multiplier.three}
+              getValue={this.handleInputChange("three")}
+            />
+            <MDBInput
+              name="PTS"
+              label="pts"
+              group
+              type="number"
+              valueDefault={this.state.multiplier.pts}
+              getValue={this.handleInputChange("pts")}
+            />
+            <MDBInput
+              name="REB"
+              label="reb"
+              group
+              type="number"
+              valueDefault={this.state.multiplier.reb}
+              getValue={this.handleInputChange("reb")}
+            />
+            <MDBInput
+              name="AST"
+              label="ast"
+              group
+              type="number"
+              valueDefault={this.state.multiplier.ast}
+              getValue={this.handleInputChange("ast")}
+            />
+            <MDBInput
+              name="STL"
+              label="stl"
+              group
+              type="number"
+              valueDefault={this.state.multiplier.stl}
+              getValue={this.handleInputChange("stl")}
+            />
+            <MDBInput
+              name="BLK"
+              label="blk"
+              group
+              type="number"
+              valueDefault={this.state.multiplier.blk}
+              getValue={this.handleInputChange("blk")}
+            />
+            <MDBInput
+              name="TO"
+              label="to"
+              group
+              type="number"
+              valueDefault={this.state.multiplier.to}
+              getValue={this.handleInputChange("to")}
+            />
+        </form>
+        </MDBCol>
+
+      </MDBRow>
+      </MDBContainer>
+
+      <MDBModal isOpen={this.state.modal_team_overview} toggle={this.toggleModal("team_overview")} size="lg">
+        <MDBModalHeader toggle={this.toggleModal("team_overview")}>Team Overview</MDBModalHeader>
+        <MDBModalBody>
+          <MDBDataTable
+            autoWidth
+            hover
+            striped
+            bordered
+            hover
+            small
+            paging={false}
+            searching={false}
+            info={false}
+            data={ {columns : modalColumns, rows : this.state.team_totals } }
+            />
+        </MDBModalBody>
+        <MDBModalFooter>
+          <MDBBtn color="secondary" onClick={this.toggleModal("team_overview")}>Close</MDBBtn>
+        </MDBModalFooter>
+      </MDBModal>
+
+      {positions.map((pos, index) => (
+        <MDBModal isOpen={this.state["modal_tiers_" + pos.abbreviation]} toggle={this.toggleModal("tiers_" + pos.abbreviation)} fullHeight position="right">
+          <MDBModalHeader toggle={this.toggleModal("tiers_" + pos.abbreviation)}>{pos.name}s Remaining:</MDBModalHeader>
+          <MDBModalBody>
+              <MDBTable small>
+                <MDBTableHead>
+                  <tr>
+                    <th>Tier</th>
+                    <th>Players</th>
+                    <th>Remaining</th>
+                  </tr>
+                </MDBTableHead>
+                <MDBTableBody>
+                  {this.state.tiers[pos.abbreviation].map((tier, index) => (
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td><ul>{tier.map((player, index) => (<li>{player}</li>))}</ul></td>
+                      <td>{tier.length}</td>
+                    </tr>
+                  ))}
+                </MDBTableBody>
+              </MDBTable>
+          </MDBModalBody>
+          <MDBModalFooter>
+            <MDBBtn color="secondary" onClick={this.toggleModal("tiers_" + pos.abbreviation)}>Close</MDBBtn>
+          </MDBModalFooter>
+        </MDBModal>
+      ))}
+      </React.Fragment>
+  );
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
